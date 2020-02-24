e6c25ce78e854182dd2762222e70cdb79b5ec815
celikelozdinc <ozdinc.celikel@siemens.com>
2020-02-24 23:14:08 +0100

    statistics for #events=900

--- plot.py
+++ plot.py
@@ -5,6 +5,8 @@ import numpy as np
 from ExperimentCluster import ExperimentCluster
 
 def main():
+
+    """
     df = pd.read_csv(u'data/300Msg.csv')
 
     _4ReplicaSet_300Events_OurSoln = ExperimentCluster(replicas=4,events=300,dataframe=df[ (df[u'ReplicaSet'] == 4) & (df[u'Solution'] == u'our') ])
@@ -16,6 +18,19 @@ def main():
 
     _12ReplicaSet_300Events_OurSoln = ExperimentCluster(replicas=12,events=300,dataframe=df[ (df[u'ReplicaSet'] == 12) & (df[u'Solution'] == u'our') ])
     _12ReplicaSet_300Events_BaseSoln = ExperimentCluster(replicas=12,events=300,dataframe=df[ (df[u'ReplicaSet'] == 12) & (df[u'Solution'] == u'base') ])
+    """
+
+
+    df_900 = pd.read_csv(u'data/900Msg.csv')
+    
+    _4ReplicaSet_900Events_OurSoln = ExperimentCluster(replicas=4,events=900,dataframe=df_900[ (df_900[u'ReplicaSet'] == 4) & (df_900[u'Solution'] == u'our') ])
+    _4ReplicaSet_900Events_BaseSoln = ExperimentCluster(replicas=4,events=900,dataframe=df_900[ (df_900[u'ReplicaSet'] == 4) & (df_900[u'Solution'] == u'base') ])
+
+    _8ReplicaSet_900Events_OurSoln = ExperimentCluster(replicas=8,events=900,dataframe=df_900[ (df_900[u'ReplicaSet'] == 8) & (df_900[u'Solution'] == u'our') ])
+    _8ReplicaSet_900Events_BaseSoln = ExperimentCluster(replicas=8,events=900,dataframe=df_900[ (df_900[u'ReplicaSet'] == 8) & (df_900[u'Solution'] == u'base') ])
+
+    _12ReplicaSet_900Events_OurSoln = ExperimentCluster(replicas=12,events=900,dataframe=df_900[ (df_900[u'ReplicaSet'] == 12) & (df_900[u'Solution'] == u'our') ])
+    _12ReplicaSet_900Events_BaseSoln = ExperimentCluster(replicas=12,events=900,dataframe=df_900[ (df_900[u'ReplicaSet'] == 12) & (df_900[u'Solution'] == u'base') ])
 
 
 
@@ -26,18 +41,18 @@ def main():
     width = 0.35
 
 
-    """
-    base_memoryfootprint_1 = _4ReplicaSet_300Events_BaseSoln._all_memory_footprint.values
-    our_memoryfootprint_1 = _4ReplicaSet_300Events_OurSoln._all_memory_footprint.values
+
+    base_memoryfootprint_1 = _4ReplicaSet_900Events_BaseSoln._all_memory_footprint.values
+    our_memoryfootprint_1 = _4ReplicaSet_900Events_OurSoln._all_memory_footprint.values
     sub_1 = base_memoryfootprint_1 - our_memoryfootprint_1
 
-    base_memoryfootprint_2 = _8ReplicaSet_300Events_BaseSoln._all_memory_footprint.values
-    our_memoryfootprint_2 = _8ReplicaSet_300Events_OurSoln._all_memory_footprint.values
+    base_memoryfootprint_2 = _8ReplicaSet_900Events_BaseSoln._all_memory_footprint.values
+    our_memoryfootprint_2 = _8ReplicaSet_900Events_OurSoln._all_memory_footprint.values
     sub_2 = base_memoryfootprint_2 - our_memoryfootprint_2
 
 
-    base_memoryfootprint_3 = _12ReplicaSet_300Events_BaseSoln._all_memory_footprint.values
-    our_memoryfootprint_3 = _12ReplicaSet_300Events_OurSoln._all_memory_footprint.values
+    base_memoryfootprint_3 = _12ReplicaSet_900Events_BaseSoln._all_memory_footprint.values
+    our_memoryfootprint_3 = _12ReplicaSet_900Events_OurSoln._all_memory_footprint.values
     sub_3 = base_memoryfootprint_3 - our_memoryfootprint_3
 
     
@@ -52,15 +67,15 @@ def main():
     plt.xticks(ind + width / 2, ('#replicas=4', '#replicas=8', '#replicas=12', '#replicas=16'))
     plt.legend(loc='upper right')
     plt.show() 
+    
+    
+    
     """
-    
-    
-    
-    green_bar = (_4ReplicaSet_300Events_BaseSoln._mean_restore_duration, _8ReplicaSet_300Events_BaseSoln._mean_restore_duration,_12ReplicaSet_300Events_BaseSoln._mean_restore_duration)
-    blue_bar = (_4ReplicaSet_300Events_OurSoln._mean_restore_duration, _8ReplicaSet_300Events_OurSoln._mean_restore_duration,_12ReplicaSet_300Events_OurSoln._mean_restore_duration)
+    green_bar = (_4ReplicaSet_900Events_BaseSoln._mean_restore_duration, _8ReplicaSet_900Events_BaseSoln._mean_restore_duration,_12ReplicaSet_900Events_BaseSoln._mean_restore_duration)
+    blue_bar = (_4ReplicaSet_900Events_OurSoln._mean_restore_duration, _8ReplicaSet_900Events_OurSoln._mean_restore_duration,_12ReplicaSet_900Events_OurSoln._mean_restore_duration)
 
-    green_std = (_4ReplicaSet_300Events_BaseSoln._std_restore_duration,_8ReplicaSet_300Events_BaseSoln._std_restore_duration,_12ReplicaSet_300Events_BaseSoln._std_restore_duration)
-    blue_std = (_4ReplicaSet_300Events_OurSoln._std_restore_duration,_8ReplicaSet_300Events_OurSoln._std_restore_duration,_12ReplicaSet_300Events_OurSoln._std_restore_duration)
+    green_std = (_4ReplicaSet_900Events_BaseSoln._std_restore_duration,_8ReplicaSet_900Events_BaseSoln._std_restore_duration,_12ReplicaSet_900Events_BaseSoln._std_restore_duration)
+    blue_std = (_4ReplicaSet_900Events_OurSoln._std_restore_duration,_8ReplicaSet_900Events_OurSoln._std_restore_duration,_12ReplicaSet_900Events_OurSoln._std_restore_duration)
 
     plt.bar(ind, green_bar , width, label='Solution With Zookeeper',color='g',yerr=green_std,align='center', ecolor='red', capsize=10)
     plt.bar(ind + width, blue_bar, width, label='Solution Without Zookeeper',color='b',yerr=blue_std,align='center', ecolor='red', capsize=10)
@@ -68,8 +83,9 @@ def main():
     plt.xlabel('Number of replicas')
     plt.title('Number of replicas vs Restore Duration')
     plt.xticks(ind + width / 2, ('#replicas=4', '#replicas=8', '#replicas=12', '#replicas=16'))
-    plt.legend(loc='upper right')
+    plt.legend(loc='upper left')
     plt.show() 
+    """
     
 
 
