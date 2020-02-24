8589a2a561d82e581022ce1cf467c9af011acba9
celikelozdinc <ozdinc.celikel@siemens.com>
2020-02-24 23:12:46 +0100

    update archive

--- archive_02.04.2020/report.py
+++ archive_02.04.2020/report.py
@@ -50,11 +50,11 @@ def generate_plot_for_memory_footprint(_4ReplicaSet, _8ReplicaSet, _12ReplicaSet
     default_std = (_4ReplicaSet._memoryFootprint_delta_std, _8ReplicaSet._memoryFootprint_delta_std, _12ReplicaSet._memoryFootprint_delta_std, _16ReplicaSet._memoryFootprint_delta_std)
     """
 
-    green_bar = (_4ReplicaSet._memoryFootprint_delta_mean, _8ReplicaSet._memoryFootprint_delta_mean, _12ReplicaSet._memoryFootprint_delta_mean)
-    default_std = (_4ReplicaSet._memoryFootprint_delta_std, _8ReplicaSet._memoryFootprint_delta_std, _12ReplicaSet._memoryFootprint_delta_std)
+    green_bar = (_4ReplicaSet._memoryFootprint_delta_mean, _8ReplicaSet._memoryFootprint_delta_mean, _12ReplicaSet._memoryFootprint_delta_mean,_16ReplicaSet._memoryFootprint_delta_mean)
+    default_std = (_4ReplicaSet._memoryFootprint_delta_std, _8ReplicaSet._memoryFootprint_delta_std, _12ReplicaSet._memoryFootprint_delta_std, _16ReplicaSet._memoryFootprint_delta_std)
 
     # the x locations for the groups
-    ind = np.arange(3)
+    ind = np.arange(4)
 
     # Figure size
     #plt.figure(figsize=(10,5))
@@ -63,23 +63,32 @@ def generate_plot_for_memory_footprint(_4ReplicaSet, _8ReplicaSet, _12ReplicaSet
     width = 0.35       
 
     # Plotting
-    plt.bar(ind, green_bar , width, label='Delta',color='g',yerr=default_std,align='center', ecolor='red', capsize=10)
+    plt.bar(ind, green_bar , width, label='Memory consumption difference',color='0.15',yerr=default_std,align='center', ecolor='red', capsize=10)
     #plt.bar(ind + width, blue_bar, width, label='Solution Without Zookeeper',color='b',yerr=our_std,align='center', ecolor='red', capsize=10)
-    plt.ylabel('Memory Footprint Delta (KB)')
-    plt.xlabel('Number of replicas')
-    plt.title('Number of replicas vs Memory Footprint Delta')
-    plt.xticks(ind + width / 2, ('#replicas=4', '#replicas=8', '#replicas=12' , '#replicas=16'))
-    plt.legend(loc='best')
+    plt.ylabel('Memory Footprint Delta (KB)',fontsize=20)
+    plt.xlabel('Number of replicas',fontsize=20)
+    #plt.title('Number of replicas vs Memory Footprint Delta')
+    plt.xticks(ind + width / 10, ('#replicas=4', '#replicas=8', '#replicas=12' , '#replicas=16'),fontsize=20)
+    plt.legend(loc='best',fontsize=20)
     plt.show()    
 
 def generate_plot_for_duration(_4ReplicaSet, _8ReplicaSet, _12ReplicaSet, _16ReplicaSet):
     print(u'Plot for duration')
 
+    
     green_bar = (_4ReplicaSet._duration_mean_with_ZK, _8ReplicaSet._duration_mean_with_ZK, _12ReplicaSet._duration_mean_with_ZK, _16ReplicaSet._duration_mean_with_ZK)
     blue_bar = (_4ReplicaSet._duration_mean_without_ZK, _8ReplicaSet._duration_mean_without_ZK, _12ReplicaSet._duration_mean_without_ZK,_16ReplicaSet._duration_mean_without_ZK)
     default_std = (_4ReplicaSet._duration_std_with_ZK, _8ReplicaSet._duration_std_with_ZK, _12ReplicaSet._duration_std_with_ZK,_16ReplicaSet._duration_std_with_ZK)
     our_std = (_4ReplicaSet._duration_std_without_ZK, _8ReplicaSet._duration_std_without_ZK, _12ReplicaSet._duration_std_without_ZK, _16ReplicaSet._duration_std_without_ZK)
     
+
+    """
+    green_bar = (_4ReplicaSet._duration_mean_with_ZK, _8ReplicaSet._duration_mean_with_ZK, _16ReplicaSet._duration_mean_with_ZK)
+    blue_bar = (_4ReplicaSet._duration_mean_without_ZK, _8ReplicaSet._duration_mean_without_ZK,_16ReplicaSet._duration_mean_without_ZK)
+    default_std = (_4ReplicaSet._duration_std_with_ZK, _8ReplicaSet._duration_std_with_ZK, _16ReplicaSet._duration_std_with_ZK)
+    our_std = (_4ReplicaSet._duration_std_without_ZK, _8ReplicaSet._duration_std_without_ZK, _16ReplicaSet._duration_std_without_ZK)
+    """
+
 
     """
     green_bar = (_4ReplicaSet._duration_mean_with_ZK, _8ReplicaSet._duration_mean_with_ZK, _12ReplicaSet._duration_mean_with_ZK)
@@ -98,22 +107,23 @@ def generate_plot_for_duration(_4ReplicaSet, _8ReplicaSet, _12ReplicaSet, _16Rep
     width = 0.35       
 
     # Plotting
-    plt.bar(ind, green_bar , width, label='Solution With Zookeeper',color='g',yerr=default_std,align='center', ecolor='red', capsize=10)
-    plt.bar(ind + width, blue_bar, width, label='Solution Without Zookeeper',color='b',yerr=our_std,align='center', ecolor='red', capsize=10)
-    plt.ylabel('Restore Duration(sec)')
-    plt.xlabel('Number of replicas')
-    plt.title('Number of replicas vs Restore Duration')
-    plt.xticks(ind + width / 2, ('#replicas=4', '#replicas=8', '#replicas=12', '#replicas=16'))
-    plt.legend(loc='best')
+    plt.bar(ind, green_bar , width, label='Solution With Zookeeper',color='0.15',yerr=default_std,align='center', ecolor='red', capsize=10)
+    plt.bar(ind + width, blue_bar, width, label='Solution Without Zookeeper',color='0.90',yerr=our_std,align='center', ecolor='red', capsize=10)
+    plt.ylabel('Restore Duration(sec)',fontsize=20)
+    plt.xlabel('Number of replicas',fontsize=20)
+    #plt.title('Number of replicas vs Restore Duration')
+    plt.xticks(ind + width / 2, ('#replicas=4', '#replicas=8', '#replicas=12','#replicas=16'),fontsize=20)
+    plt.legend(loc='best',fontsize=20)
     plt.show()    
 
 def main(): 
+    
     """
-    _96Event_4Replicas = Storage(); _96Event_4Replicas._populate_data('4_Without_ZK.csv',u'4_With_ZK.csv'); _96Event_4Replicas._calculate_statistics()
-    _96Event_8Replicas = Storage(); _96Event_8Replicas._populate_data('8_Without_ZK.csv',u'8_With_ZK.csv'); _96Event_8Replicas._calculate_statistics()
-    _96Event_12Replicas = Storage(); _96Event_12Replicas._populate_data('12_Without_ZK.csv',u'12_With_ZK.csv'); _96Event_12Replicas._calculate_statistics()
-    _96Event_16Replicas = Storage(); _96Event_16Replicas._populate_data('16_Without_ZK.csv',u'16_With_ZK.csv'); _96Event_16Replicas._calculate_statistics()
-    #generate_plot_for_duration(_96Event_4Replicas, _96Event_8Replicas, _96Event_12Replicas, _96Event_16Replicas)
+    _96Event_4Replicas = Storage(); _96Event_4Replicas._populate_data('our_solution/96_4.csv','base_solution/96_4.csv'); _96Event_4Replicas._calculate_statistics()
+    _96Event_8Replicas = Storage(); _96Event_8Replicas._populate_data('our_solution/96_8.csv','base_solution/96_8.csv'); _96Event_8Replicas._calculate_statistics()
+    _96Event_12Replicas = Storage(); _96Event_12Replicas._populate_data('our_solution/96_12.csv','base_solution/96_12.csv'); _96Event_12Replicas._calculate_statistics()
+    _96Event_16Replicas = Storage(); _96Event_16Replicas._populate_data('our_solution/96_16.csv','base_solution/96_16.csv'); _96Event_16Replicas._calculate_statistics()
+    generate_plot_for_duration(_96Event_4Replicas, _96Event_8Replicas, _96Event_12Replicas, _96Event_16Replicas)
     """
 
     """
@@ -125,16 +135,16 @@ def main():
     #generate_plot_for_memory_footprint(_9600Event_4Replicas, _9600Event_8Replicas, _9600Event_12Replicas,"")
     """
 
-    """
-    _960Event_4Replicas = Storage(); _960Event_4Replicas._populate_data('960_4_Without_ZK.csv',u'960_4_With_ZK.csv'); _960Event_4Replicas._calculate_statistics()
-    _960Event_8Replicas = Storage(); _960Event_8Replicas._populate_data('960_8_Without_ZK.csv',u'960_8_With_ZK.csv'); _960Event_8Replicas._calculate_statistics()
-    _960Event_12Replicas = Storage(); _960Event_12Replicas._populate_data('960_12_Without_ZK.csv',u'960_12_With_ZK.csv'); _960Event_12Replicas._calculate_statistics()
-    _960Event_16Replicas = Storage(); _960Event_16Replicas._populate_data('960_16_Without_ZK.csv',u'960_16_With_ZK.csv'); _960Event_16Replicas._calculate_statistics()
+    
+    _960Event_4Replicas = Storage(); _960Event_4Replicas._populate_data('our_solution/960_4.csv','base_solution/960_4.csv'); _960Event_4Replicas._calculate_statistics()
+    _960Event_8Replicas = Storage(); _960Event_8Replicas._populate_data('our_solution/960_8.csv','base_solution/960_8.csv'); _960Event_8Replicas._calculate_statistics()
+    _960Event_12Replicas = Storage(); _960Event_12Replicas._populate_data('our_solution/960_12.csv','base_solution/960_12.csv'); _960Event_12Replicas._calculate_statistics()
+    _960Event_16Replicas = Storage(); _960Event_16Replicas._populate_data('our_solution/960_16.csv','base_solution/960_16.csv'); _960Event_16Replicas._calculate_statistics()
     #generate_plot_for_duration(_960Event_4Replicas, _960Event_8Replicas, _960Event_12Replicas, _960Event_16Replicas)
-    #generate_plot_for_memory_footprint(_960Event_4Replicas, _960Event_8Replicas, _960Event_12Replicas, _960Event_16Replicas)
-    """
+    generate_plot_for_memory_footprint(_960Event_4Replicas, _960Event_8Replicas, _960Event_12Replicas, _960Event_16Replicas)
+    
 
-
+"""
     # %%
     _4800Event_4Replicas = Storage(); _4800Event_4Replicas._populate_data(u'our_solution/4800_4.csv',u'base_solution/4800_4.csv'); _4800Event_4Replicas._calculate_statistics()
     _4800Event_8Replicas = Storage(); _4800Event_8Replicas._populate_data(u'our_solution/4800_8.csv',u'base_solution/4800_8.csv'); _4800Event_8Replicas._calculate_statistics()
@@ -142,7 +152,8 @@ def main():
     _4800Event_16Replicas = Storage(); _4800Event_16Replicas._populate_data(u'our_solution/4800_16.csv',u'base_solution/4800_16.csv'); _4800Event_16Replicas._calculate_statistics()
     generate_plot_for_duration(_4800Event_4Replicas, _4800Event_8Replicas, _4800Event_12Replicas, _4800Event_16Replicas)
     #generate_plot_for_memory_footprint(_4800Event_4Replicas, _4800Event_8Replicas, _4800Event_12Replicas, _4800Event_16Replicas)
-    
+"""    
+
 
 """
     _2400Event_4Replicas = Storage(); _2400Event_4Replicas._populate_data(u'our_solution/2400_4.csv',u'base_solution/2400_4.csv'); _2400Event_4Replicas._calculate_statistics()
