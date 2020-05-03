04a8e79b8d6efbe0306bce275c4ab631f3c56f56
celikelozdinc <ozdinc.celikel@siemens.com>
2020-05-03 16:01:17 +0300

    statistics for 12 replicas

--- plot.py
+++ plot.py
@@ -20,7 +20,7 @@ def main():
     _12ReplicaSet_300Events_BaseSoln = ExperimentCluster(replicas=12,events=300,dataframe=df[ (df[u'ReplicaSet'] == 12) & (df[u'Solution'] == u'base') ])
     """
 
-    df_6000 = pd.read_csv("data/6000Msg_newcomputer.csv")
+    df_6000 = pd.read_csv("data/6000Msg_with_12.csv")
     _4ReplicaSet_6000Events_DistributedSoln = ExperimentCluster(replicas=4,events=6000,dataframe=df_6000[ (df_6000["ReplicaSet"] == 4) & (df_6000["Solution"] == "distributed") ])
     _4ReplicaSet_6000Events_CentralizedSoln = ExperimentCluster(replicas=4,events=6000,dataframe=df_6000[ (df_6000["ReplicaSet"] == 4) & (df_6000["Solution"] == "centralized") ])
     _4ReplicaSet_6000Events_ConventionalSoln = ExperimentCluster(replicas=4,events=6000,dataframe=df_6000[ (df_6000["ReplicaSet"] == 4) & (df_6000["Solution"] == "conventional") ])
@@ -37,6 +37,9 @@ def main():
     _10ReplicaSet_6000Events_CentralizedSoln = ExperimentCluster(replicas=10,events=6000,dataframe=df_6000[ (df_6000["ReplicaSet"] == 10) & (df_6000["Solution"] == "centralized") ])
     _10ReplicaSet_6000Events_ConventionalSoln = ExperimentCluster(replicas=10,events=6000,dataframe=df_6000[ (df_6000["ReplicaSet"] == 10) & (df_6000["Solution"] == "conventional") ])
 
+    _12ReplicaSet_6000Events_DistributedSoln = ExperimentCluster(replicas=12,events=6000,dataframe=df_6000[ (df_6000["ReplicaSet"] == 12) & (df_6000["Solution"] == "distributed") ])
+    _12ReplicaSet_6000Events_CentralizedSoln = ExperimentCluster(replicas=12,events=6000,dataframe=df_6000[ (df_6000["ReplicaSet"] == 12) & (df_6000["Solution"] == "centralized") ])
+    _12ReplicaSet_6000Events_ConventionalSoln = ExperimentCluster(replicas=12,events=6000,dataframe=df_6000[ (df_6000["ReplicaSet"] == 12) & (df_6000["Solution"] == "conventional") ])
 
 
     # the width of the bars
@@ -54,17 +57,17 @@ def main():
 
     # heights of the bars    
     
-    #green_bar = (_4ReplicaSet_6000Events_DistributedSoln._mean_restore_duration,_6ReplicaSet_6000Events_DistributedSoln._mean_restore_duration,_8ReplicaSet_6000Events_DistributedSoln._mean_restore_duration,_10ReplicaSet_6000Events_DistributedSoln._mean_restore_duration)
-    #blue_bar = (_4ReplicaSet_6000Events_CentralizedSoln._mean_restore_duration, _6ReplicaSet_6000Events_CentralizedSoln._mean_restore_duration,_8ReplicaSet_6000Events_CentralizedSoln._mean_restore_duration,_10ReplicaSet_6000Events_CentralizedSoln._mean_restore_duration)
-    #new_bar = (_4ReplicaSet_6000Events_ConventionalSoln._mean_restore_duration, _6ReplicaSet_6000Events_ConventionalSoln._mean_restore_duration,_8ReplicaSet_6000Events_ConventionalSoln._mean_restore_duration, _10ReplicaSet_6000Events_ConventionalSoln._mean_restore_duration)
-    #all_bars = green_bar + blue_bar + new_bar
+    green_bar = (_6ReplicaSet_6000Events_DistributedSoln._mean_restore_duration,_8ReplicaSet_6000Events_DistributedSoln._mean_restore_duration,_10ReplicaSet_6000Events_DistributedSoln._mean_restore_duration,_12ReplicaSet_6000Events_DistributedSoln._mean_restore_duration)
+    blue_bar = (_6ReplicaSet_6000Events_CentralizedSoln._mean_restore_duration,_8ReplicaSet_6000Events_CentralizedSoln._mean_restore_duration,_10ReplicaSet_6000Events_CentralizedSoln._mean_restore_duration,_12ReplicaSet_6000Events_CentralizedSoln._mean_restore_duration)
+    new_bar = (_6ReplicaSet_6000Events_ConventionalSoln._mean_restore_duration,_8ReplicaSet_6000Events_ConventionalSoln._mean_restore_duration, _10ReplicaSet_6000Events_ConventionalSoln._mean_restore_duration, _12ReplicaSet_6000Events_ConventionalSoln._mean_restore_duration)
+    all_bars = green_bar + blue_bar + new_bar
    
 
 
-    green_bar = (_4ReplicaSet_6000Events_DistributedSoln._mean_memory_footprint,_6ReplicaSet_6000Events_DistributedSoln._mean_memory_footprint,_8ReplicaSet_6000Events_DistributedSoln._mean_memory_footprint,_10ReplicaSet_6000Events_DistributedSoln._mean_memory_footprint)
-    blue_bar = (_4ReplicaSet_6000Events_CentralizedSoln._mean_memory_footprint, _6ReplicaSet_6000Events_CentralizedSoln._mean_memory_footprint,_8ReplicaSet_6000Events_CentralizedSoln._mean_memory_footprint,_10ReplicaSet_6000Events_CentralizedSoln._mean_memory_footprint)
-    new_bar = (_4ReplicaSet_6000Events_ConventionalSoln._mean_memory_footprint, _6ReplicaSet_6000Events_ConventionalSoln._mean_memory_footprint,_8ReplicaSet_6000Events_ConventionalSoln._mean_memory_footprint,_10ReplicaSet_6000Events_ConventionalSoln._mean_memory_footprint)
-    all_bars = green_bar + blue_bar + new_bar
+    #green_bar = (_6ReplicaSet_6000Events_DistributedSoln._mean_memory_footprint,_8ReplicaSet_6000Events_DistributedSoln._mean_memory_footprint,_10ReplicaSet_6000Events_DistributedSoln._mean_memory_footprint,_12ReplicaSet_6000Events_DistributedSoln._mean_memory_footprint)
+    #blue_bar = (_6ReplicaSet_6000Events_CentralizedSoln._mean_memory_footprint,_8ReplicaSet_6000Events_CentralizedSoln._mean_memory_footprint,_10ReplicaSet_6000Events_CentralizedSoln._mean_memory_footprint,_12ReplicaSet_6000Events_CentralizedSoln._mean_memory_footprint)
+    #new_bar = (_6ReplicaSet_6000Events_ConventionalSoln._mean_memory_footprint,_8ReplicaSet_6000Events_ConventionalSoln._mean_memory_footprint,_10ReplicaSet_6000Events_ConventionalSoln._mean_memory_footprint, _12ReplicaSet_6000Events_ConventionalSoln._mean_memory_footprint)
+    #all_bars = green_bar + blue_bar + new_bar
 
     
     print("Distributed Checkpointing bar: {}".format(green_bar))
@@ -73,15 +76,16 @@ def main():
     print("All bars: {}".format(all_bars))
 
     # std errors
-    #green_std = (_4ReplicaSet_6000Events_DistributedSoln._std_restore_duration,_6ReplicaSet_6000Events_DistributedSoln._std_restore_duration,_8ReplicaSet_6000Events_DistributedSoln._std_restore_duration,_10ReplicaSet_6000Events_DistributedSoln._std_restore_duration)
-    #blue_std = (_4ReplicaSet_6000Events_CentralizedSoln._std_restore_duration,_6ReplicaSet_6000Events_CentralizedSoln._std_restore_duration,_8ReplicaSet_6000Events_CentralizedSoln._std_restore_duration,_10ReplicaSet_6000Events_CentralizedSoln._std_restore_duration)
-    #new_std = (_4ReplicaSet_6000Events_ConventionalSoln._std_restore_duration, _6ReplicaSet_6000Events_ConventionalSoln._std_restore_duration,_8ReplicaSet_6000Events_ConventionalSoln._std_restore_duration,_10ReplicaSet_6000Events_ConventionalSoln._std_restore_duration)
+    green_std = (_6ReplicaSet_6000Events_DistributedSoln._std_restore_duration,_8ReplicaSet_6000Events_DistributedSoln._std_restore_duration,_10ReplicaSet_6000Events_DistributedSoln._std_restore_duration, _12ReplicaSet_6000Events_DistributedSoln._std_restore_duration)
+    blue_std = (_6ReplicaSet_6000Events_CentralizedSoln._std_restore_duration,_8ReplicaSet_6000Events_CentralizedSoln._std_restore_duration,_10ReplicaSet_6000Events_CentralizedSoln._std_restore_duration, _12ReplicaSet_6000Events_CentralizedSoln._std_restore_duration)
+    new_std = (_6ReplicaSet_6000Events_ConventionalSoln._std_restore_duration,_8ReplicaSet_6000Events_ConventionalSoln._std_restore_duration,_10ReplicaSet_6000Events_ConventionalSoln._std_restore_duration, _12ReplicaSet_6000Events_ConventionalSoln._std_restore_duration)
     
 
+    #green_std = (_6ReplicaSet_6000Events_DistributedSoln._std_memory_footprint,_8ReplicaSet_6000Events_DistributedSoln._std_memory_footprint,_10ReplicaSet_6000Events_DistributedSoln._std_memory_footprint, _12ReplicaSet_6000Events_DistributedSoln._std_memory_footprint)
+    #blue_std = (_6ReplicaSet_6000Events_CentralizedSoln._std_memory_footprint,_8ReplicaSet_6000Events_CentralizedSoln._std_memory_footprint,_10ReplicaSet_6000Events_CentralizedSoln._std_memory_footprint,_12ReplicaSet_6000Events_CentralizedSoln._std_memory_footprint)
+    #new_std = (_6ReplicaSet_6000Events_ConventionalSoln._std_memory_footprint,_8ReplicaSet_6000Events_ConventionalSoln._std_memory_footprint,_10ReplicaSet_6000Events_ConventionalSoln._std_memory_footprint, _12ReplicaSet_6000Events_ConventionalSoln._std_memory_footprint)
     
-    green_std = (_4ReplicaSet_6000Events_DistributedSoln._std_memory_footprint,_6ReplicaSet_6000Events_DistributedSoln._std_memory_footprint,_8ReplicaSet_6000Events_DistributedSoln._std_memory_footprint,_10ReplicaSet_6000Events_DistributedSoln._std_memory_footprint)
-    blue_std = (_4ReplicaSet_6000Events_CentralizedSoln._std_memory_footprint,_6ReplicaSet_6000Events_CentralizedSoln._std_memory_footprint,_8ReplicaSet_6000Events_CentralizedSoln._std_memory_footprint,_10ReplicaSet_6000Events_CentralizedSoln._std_memory_footprint)
-    new_std = (_4ReplicaSet_6000Events_ConventionalSoln._std_memory_footprint, _6ReplicaSet_6000Events_ConventionalSoln._std_memory_footprint,_8ReplicaSet_6000Events_ConventionalSoln._std_memory_footprint,_10ReplicaSet_6000Events_ConventionalSoln._std_memory_footprint)
+    
     print("STD of Distributed Checkpointing bar: {}".format(green_std))
     print("STD of Centralized Checkpointing bar: {}".format(blue_std))
     print("STD of Conventional Checkpointing bar: {}".format(new_std))
@@ -98,34 +102,34 @@ def main():
     print("Position of bars - r4 : {}".format(r4))
 
     # Make the plot
-    """
     plt.bar(r1, green_bar, width=barWidth, label='Distributed Checkpointing',color='0.30',yerr=green_std,align='center', ecolor='red', capsize=10)
     plt.bar(r2, blue_bar, width=barWidth, label='Centralized Checkpointing',color='0.60',yerr=blue_std,align='center', ecolor='red', capsize=10)
     plt.bar(r3, new_bar, width=barWidth, label='Conventional Checkpointing', color ='0.90', yerr=new_std, align='center', ecolor='red',capsize=10)
     plt.ylabel('Restore Duration(sec)',fontsize=10,fontweight="bold")
     plt.xlabel('Number of replicas',fontsize=10,fontweight="bold")
     #plt.title('Number of replicas vs Restore Duration')
-    plt.xticks([r + barWidth for r in range(4)], ('#replicas=4', '#replicas=6', '#replicas=8', '#replicas=10'),fontsize=10, rotation=45)
+    plt.xticks([r + barWidth for r in range(4)], ('#replicas=6', '#replicas=8', '#replicas=10', '#replicas=12'),fontsize=10, rotation=45)
     for i in range(len(r4)):
         plt.text(x = r4[i]-0.1, y = all_bars[i]+0.2, s = all_bars[i], size = 6)
     plt.legend(loc='best',fontsize=10)
     plt.show()
-    """
+    
     
     
 
-    
+    """
     plt.bar(r1, green_bar, width=barWidth, label='Distributed Checkpointing',color='0.30',yerr=green_std,align='center', ecolor='red', capsize=10)
     plt.bar(r2, blue_bar, width=barWidth, label='Centralized Checkpointing',color='0.60',yerr=blue_std,align='center', ecolor='red', capsize=10)
     plt.bar(r3, new_bar, width=barWidth, label='Conventional Checkpointing', color ='0.90', yerr=new_std, align='center', ecolor='red',capsize=10)
     plt.ylabel('Memory Footprint(kiB)',fontsize=10,fontweight="bold")
     plt.xlabel('Number of replicas',fontsize=10,fontweight="bold")
     #plt.title('Number of replicas vs Restore Duration')
-    plt.xticks([r + barWidth for r in range(4)], ('#replicas=4', '#replicas=6','#replicas=8','#replicas=10'),fontsize=10,rotation=45)
+    plt.xticks([r + barWidth for r in range(4)], ('#replicas=6','#replicas=8','#replicas=10','#replicas=12'),fontsize=10,rotation=45)
     for i in range(len(r4)):
         plt.text(x = r4[i]-0.1, y = all_bars[i]+0.2, s = all_bars[i], size = 6)
     plt.legend(loc='best',fontsize=10)
     plt.show()
+    """
     
     
     
